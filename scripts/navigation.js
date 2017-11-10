87e850a60b9d9dfb3c8df158d6346875771777c8
Jaber Ibne Mahboob <jaber.ibne.mahboob@gmail.com>
2017-11-10 14:44:27 +0100

    sidebar

---
+++ scripts/navigation.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.querySelector(".pageNavigation").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.querySelector(".pageNavigation").style.width = "0px";
+}
